de69bcab4471b371be37efe3e2541853750f886c
saadaoui-salah <salahsaadaoui8@gmail.com>
2026-03-09 05:15:17 +0100

    update

--- core/spiders/brickeconomy.py
+++ core/spiders/brickeconomy.py
@@ -154,7 +154,7 @@ class BrickeconomySpider(scrapy.Spider):
                 except:
                     pass
             item['sellers'] += [{
-                'usd_price':float(seller.css('.a::text').get('').replace('$','')),
+                'usd_price':float(seller.css('.a::text').get('').replace('$','').replace(',','')),
                 'price_change':seller.css('div.text-small::text').get(),
                 'condition':'Used',
                 'country':seller.css('::attr(data-region)').get(),
